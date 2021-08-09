39bda76d9a34d13f34125b480a82be412d70f0a3
MUTHUSAMYNK <88675497+MUTHUSAMYNK@users.noreply.github.com>
2021-08-09 23:19:21 +0530

    Add files via upload

---
+++ multiplesof2.js
@@ -0,0 +1,34 @@
+const readline = require("readline");
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = []
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+
+
+
+var a=userInput[0];
+
+str="";
+ 
+var sum=0;
+
+    for(i=1 ; i<4;i++)
+
+{
+
+    sum= i * a;
+   str=str+" "+sum; 
+
+    
+}    
+ 
+ 
+ console.log(str.trim(" "));
+  
+
+    
+});
